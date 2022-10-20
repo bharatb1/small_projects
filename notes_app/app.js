166ff94b0986286ab2b3510da4a99a8cb1987eff
rohitb <72206820+rohitb1@users.noreply.github.com>
2022-10-20 16:11:13 +0530

    Update app.js
    added functions

--- notes_app/app.js
+++ notes_app/app.js
@@ -3,16 +3,6 @@ const addBtn = document.getElementById("add");
 
 const notes = JSON.parse(localStorage.getItem("notes"));
 
-if (notes) {
-    notes.forEach((note) => {
-        addNewNote(note);
-    });
-}
-
-addBtn.addEventListener("click", () => {
-    addNewNote();
-});
-
 function addNewNote(text = "") {
     const note = document.createElement("div");
     note.classList.add("note");
@@ -28,6 +18,18 @@ function addNewNote(text = "") {
              <br>
         </div>
     `;
+
+if (notes) {
+    notes.forEach((note) => {
+        addNewNote(note);
+    });
+}
+
+addBtn.addEventListener("click", () => {
+    addNewNote();
+});
+
+
 
     const editBtn = note.querySelector(".edit");
     const deleteBtn = note.querySelector(".delete");
